e081905ae6d30c543901fa3e741a014dcde2f699
Ian Macalinao <me@ian.pw>
2015-04-06 03:10:54 -0500

    Add article/user associations

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150406073901) do
+ActiveRecord::Schema.define(version: 20150406080937) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
+
+  add_index "articles", ["user_id"], name: "index_articles_on_user_id"
 
   create_table "comments", force: :cascade do |t|
     t.string   "commenter"
