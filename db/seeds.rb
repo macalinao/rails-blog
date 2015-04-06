f456a90ba2d5139143b2c938974965c460348a71
Ian Macalinao <me@ian.pw>
2015-04-06 04:16:35 -0500

    Add some lorem ipsum seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+users = []
+10.times do |i|
+  users << User.create(username: Faker::Internet.user_name,
+              password: Faker::Internet.password,
+              email: Faker::Internet.safe_email)
+end
+
+50.times do |i|
+  Article.create(title: Faker::Lorem.sentence,
+                 text: Faker::Lorem.paragraphs(5).join("\n\n"),
+                 user: users[i % users.length])
+end
