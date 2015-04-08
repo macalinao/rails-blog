2fe7d708c2357bb643639cbe13b62f6eb2fd20a6
Ian Macalinao <me@ian.pw>
2015-04-08 02:27:04 -0500

    Remove scroll listener

--- app/assets/javascripts/articles.js
+++ app/assets/javascripts/articles.js
@@ -1,12 +1,12 @@
 $(document).on('page:change', function() {
   var page = 2;
   var end = false;
-  $(window).scroll(function() {
+  $(window).off('scroll').scroll(function() {
     if ($('#infiniteScroll').length === 0 || end) return;
     if($(window).scrollTop() + $(window).height() == $(document).height()) {
       $.get('/partial_page?page=' + page, function(data) {
         if (data.length !== 1) {
-          $('#articles').append(data);
+          $('#articlesExtra').append(data);
           page++;
         } else {
           $('#infiniteScroll').removeClass('hidden');
